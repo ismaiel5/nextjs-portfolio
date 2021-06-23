e81dee814155c537181772c7d3c49f2a43d2edee
ismaiel <ismaiel.altahan@gmail.com>
2021-06-23 22:10:36 +0800

    adding and configuring jest testing in the project

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+// in case we use js file then we need to use this way to export
+// ----> module.exports = {
+
+// if we use ts file then we can use this way to export 
+// ----> export default 
+export default {
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+}
