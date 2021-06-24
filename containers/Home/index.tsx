6fce3659d8307035d82a5837719b123a88f42f6e
ismaiel <ismaiel.altahan@gmail.com>
2021-06-24 23:19:16 +0800

    -added containers structure - added 2 containers - use the Home contianer in Home page and pass the data as props

---
+++ containers/Home/index.tsx
@@ -0,0 +1,10 @@
+import Styles from '../../styles/Home.module.scss';
+const Home = (props: any) => {
+  return (
+    <main className={Styles.container}>
+      <h1>Home page</h1>
+    </main>
+  );
+};
+
+export default Home;
